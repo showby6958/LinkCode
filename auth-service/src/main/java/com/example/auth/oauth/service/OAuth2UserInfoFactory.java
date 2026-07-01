18cc52b2243746e78c94b890ffc4ae28eaa28d4a
Jinho Jang <wkdwlsgh9622@gmail.com>
2026-07-01 17:33:05 +0900

    Feat: Enable Naver Social Login (OAuth 2.0)

--- auth-service/src/main/java/com/example/auth/oauth/service/OAuth2UserInfoFactory.java
+++ auth-service/src/main/java/com/example/auth/oauth/service/OAuth2UserInfoFactory.java
@@ -3,7 +3,7 @@ package com.example.auth.oauth.service;
 
 import com.example.auth.oauth.userinfo.GoogleOAuth2UserInfo;
 //import com.example.auth.oauth.userinfo.KakaoOAuth2UserInfo;
-//import com.example.auth.oauth.userinfo.NaverOAuth2UserInfo;
+import com.example.auth.oauth.userinfo.NaverOAuth2UserInfo;
 import com.example.auth.oauth.userinfo.OAuth2UserInfo;
 
 import java.util.Map;
@@ -15,10 +15,10 @@ public class OAuth2UserInfoFactory {
         switch (registrationId) {
             case "google":
                 return new GoogleOAuth2UserInfo(attributes);
+            case "naver":
+                return new NaverOAuth2UserInfo(attributes);
             case "kakao":
 //                return new KakaoOAuth2UserInfo(attributes);
-            case "naver":
-//                return new NaverOAuth2UserInfo(attributes);
             default:
                 throw new IllegalArgumentException("지원하지 않는 OAuth provider");
         }
--- auth-service/src/main/java/com/example/auth/oauth/userinfo/NaverOAuth2UserInfo.java
+++ auth-service/src/main/java/com/example/auth/oauth/userinfo/NaverOAuth2UserInfo.java
@@ -1,25 +1,35 @@
-//package com.example.auth.oauth.userinfo;
-//
-//import java.util.Map;
-//
-//public class NaverOAuth2UserInfo extends OAuth2UserInfo {
-//
-//    public NaverOAuth2UserInfo(Map<String, Object> attributes) {
-//        super((Map<String, Object>) attributes.get("response"));
-//    }
-//
-//    @Override
-//    public String getId() {
-//        return (String) attributes.get("id");
-//    }
-//
-//    @Override
-//    public String getEmail() {
-//        return (String) attributes.get("email");
-//    }
-//
-//    @Override
-//    public String getName() {
-//        return (String) attributes.get("name");
-//    }
-//}
+package com.example.auth.oauth.userinfo;
+
+import java.util.Map;
+
+public class NaverOAuth2UserInfo extends OAuth2UserInfo {
+
+    @SuppressWarnings("unchecked")
+    public NaverOAuth2UserInfo(Map<String, Object> attributes) {
+        super((Map<String, Object>) attributes.get("response"));
+    }
+
+    // 네이버가 부여한 사용자 식별 번호
+    @Override
+    public String getId() {
+        return (String) attributes.get("id");
+    }
+
+    // 사용자 이메일
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    // 사용자의 전체 이름
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    // 프로필 사진 이미지 경로
+    @Override
+    public String getPictureUrl() {
+        return (String) attributes.get("profile_image");
+    }
+}
